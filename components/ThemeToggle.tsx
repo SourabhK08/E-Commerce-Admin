d9350d47f43d26327e09241f74deb59d129fa1ea
Sourabhk08 <kakanisourabh23@gmail.com>
2025-12-26 10:09:23 +0530

    Theme toggle enabled

---
+++ components/ThemeToggle.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
+
+export default function ThemeToggle() {
+  const [dark, setDark] = useState(false);
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+      setDark(true);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (dark) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+    setDark(!dark);
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="px-4 py-2 rounded-md border dark:border-gray-700"
+    >
+      {dark ? <Sun size={24} /> : <Moon size={24} />}
+      
+    </button>
+  );
+}
